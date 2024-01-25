c88752ccd975d38ebbd9849228364938cf3c536a
Andy <andy@example.ru>
2024-01-25 21:33:18 +0500

    !

---
+++ Hello world.py
@@ -0,0 +1,6 @@
+print("If want to say, 'Hello, world'? enter 'yes', but if you dont want to say hello to the world, say 'no'")
+answer = input()
+if answer == 'yes':
+    print("Hello world")
+else:
+    print('I dont believe in that!')
